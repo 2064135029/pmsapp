d0e4354c00e7b4f850b35775b7264a107c0b402d
wujh <wujianhe@ganron.com>
2019-11-14 09:36:09 +0800

    no message

---
+++ android/app/src/main/kotlin/com/wujhe/apppms/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.wujhe.apppms
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
