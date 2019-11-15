b9d74ba4bb1ba47b98f51bc228fcd2758688cb93
wujh <wujianhe@ganron.com>
2019-11-15 17:38:56 +0800

    no message

--- lib/view/pages/HomePage.dart
+++ lib/view/pages/HomePage.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget {
+  final String text;
+  HomePage({Key key, this.text}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
